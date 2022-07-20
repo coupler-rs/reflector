48a88d2de4d9a62732b9dd5686a694970e38cc71
glowcoil <micahrjohnston@gmail.com>
2022-07-20 12:10:56 -0500

    windows: register and unregister window class

--- src/windows.rs
+++ src/windows.rs
@@ -1,18 +1,37 @@
-use crate::{AppContext, Event, Response, Result, WindowOptions};
+use crate::{AppContext, Error, Event, Response, Result, WindowOptions};
 
-use std::fmt;
+use std::ffi::OsStr;
 use std::marker::PhantomData;
+use std::os::windows::ffi::OsStrExt;
+use std::{fmt, ptr};
+
+use winapi::{
+    shared::minwindef, shared::ntdef, shared::windef, um::errhandlingapi, um::winnt, um::winuser,
+};
+
+extern "C" {
+    static __ImageBase: winnt::IMAGE_DOS_HEADER;
+}
+
+fn to_wstring(str: &str) -> Vec<ntdef::WCHAR> {
+    let mut wstr: Vec<ntdef::WCHAR> = OsStr::new(str).encode_wide().collect();
+    wstr.push(0);
+    wstr
+}
 
 #[derive(Debug)]
-pub struct OsError {}
+pub struct OsError {
+    code: minwindef::DWORD,
+}
 
 impl fmt::Display for OsError {
-    fn fmt(&self, _fmt: &mut fmt::Formatter) -> fmt::Result {
-        Ok(())
+    fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
+        write!(fmt, "{}", self.code)
     }
 }
 
 pub struct AppInner<T> {
+    class: minwindef::ATOM,
     state: T,
 }
 
@@ -22,16 +41,53 @@ impl<T> AppInner<T> {
         F: FnOnce(&AppContext<T>) -> Result<T>,
         T: 'static,
     {
+        let class = unsafe {
+            let class_name = to_wstring(&format!("window-{}", uuid::Uuid::new_v4().to_simple()));
+
+            let wnd_class = winuser::WNDCLASSW {
+                style: winuser::CS_HREDRAW | winuser::CS_VREDRAW | winuser::CS_OWNDC,
+                lpfnWndProc: Some(wnd_proc),
+                cbClsExtra: 0,
+                cbWndExtra: 0,
+                hInstance: &__ImageBase as *const winnt::IMAGE_DOS_HEADER as minwindef::HINSTANCE,
+                hIcon: ptr::null_mut(),
+                hCursor: winuser::LoadCursorW(ptr::null_mut(), winuser::IDC_ARROW),
+                hbrBackground: ptr::null_mut(),
+                lpszMenuName: ptr::null(),
+                lpszClassName: class_name.as_ptr(),
+            };
+
+            let class = winuser::RegisterClassW(&wnd_class);
+            if class == 0 {
+                return Err(Error::Os(OsError {
+                    code: errhandlingapi::GetLastError(),
+                }));
+            }
+
+            class
+        };
+
         let cx = AppContext::from_inner(AppContextInner {
             phantom: PhantomData,
         });
 
-        Ok(AppInner { state: build(&cx)? })
+        Ok(AppInner { class, state: build(&cx)? })
     }
 
     pub fn run(&self) {}
 
     pub fn poll(&self) {}
+}
+
+impl<T> Drop for AppInner<T> {
+    fn drop(&mut self) {
+        unsafe {
+            winuser::UnregisterClassW(
+                self.class as *const ntdef::WCHAR,
+                &__ImageBase as *const winnt::IMAGE_DOS_HEADER as minwindef::HINSTANCE,
+            );
+        }
+    }
 }
 
 pub struct AppContextInner<T> {
@@ -56,4 +112,13 @@ impl WindowInner {
     {
         Ok(WindowInner {})
     }
+}
+
+unsafe extern "system" fn wnd_proc(
+    hwnd: windef::HWND,
+    msg: minwindef::UINT,
+    wparam: minwindef::WPARAM,
+    lparam: minwindef::LPARAM,
+) -> minwindef::LRESULT {
+    winuser::DefWindowProcW(hwnd, msg, wparam, lparam)
 }
