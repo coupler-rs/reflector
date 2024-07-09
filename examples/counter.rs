df78567b1b4255883ff1ce41111e7b8792104fd1
Micah Johnston <micahrjohnston@gmail.com>
2024-07-09 00:50:23 -0500

    WindowOptions and Window types

--- examples/counter.rs
+++ examples/counter.rs
@@ -1,7 +1,13 @@
-use reflector::App;
+use reflector::{App, Size, WindowOptions};
 
 fn main() {
     let app = App::new().unwrap();
+
+    WindowOptions::new()
+        .title("window")
+        .size(Size::new(512.0, 512.0))
+        .open(&app)
+        .unwrap();
 
     app.run().unwrap();
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,61 @@
 use reflector_platform as platform;
 
+pub use platform::{Point, Size};
+
 mod error;
 
 pub use error::{Error, Result};
 
 pub struct App {
-    platform_app: platform::App,
+    inner: platform::App,
 }
 
 impl App {
     pub fn new() -> Result<App> {
         Ok(App {
-            platform_app: platform::App::new()?,
+            inner: platform::App::new()?,
         })
     }
 
     pub fn run(&self) -> Result<()> {
-        Ok(self.platform_app.run()?)
+        Ok(self.inner.run()?)
     }
+}
+
+#[derive(Default)]
+pub struct WindowOptions {
+    inner: platform::WindowOptions,
+}
+
+impl WindowOptions {
+    pub fn new() -> WindowOptions {
+        Self::default()
+    }
+
+    pub fn title<S: AsRef<str>>(&mut self, title: S) -> &mut Self {
+        self.inner.title(title);
+        self
+    }
+
+    pub fn position(&mut self, position: Point) -> &mut Self {
+        self.inner.position(position);
+        self
+    }
+
+    pub fn size(&mut self, size: Size) -> &mut Self {
+        self.inner.size(size);
+        self
+    }
+
+    pub fn open(&self, app: &App) -> Result<Window> {
+        let window = self.inner.open(app.inner.handle(), |_, _| platform::Response::Ignore)?;
+
+        window.show();
+
+        Ok(Window { _inner: window })
+    }
+}
+
+pub struct Window {
+    _inner: platform::Window,
 }
