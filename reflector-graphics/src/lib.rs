17d738b07f0554dbe97e3fec806999c1f198948e
Micah Johnston <micahrjohnston@gmail.com>
2024-07-23 19:42:22 -0500

    graphics: rename renderer.rs to canvas.rs

--- reflector-graphics/src/lib.rs
+++ reflector-graphics/src/lib.rs
@@ -1,14 +1,14 @@
+mod canvas;
 mod color;
 mod flatten;
 mod geom;
 mod path;
 mod raster;
-mod renderer;
 mod simd;
 mod text;
 
+pub use canvas::*;
 pub use color::*;
 pub use geom::*;
 pub use path::Path;
-pub use renderer::*;
 pub use text::*;
